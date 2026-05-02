e6457943833d3413cd05da158aabb28cae6f40e4
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-05-02 15:06:30 -0300

    components about feito

--- landingpage-oil/src/App.jsx
+++ landingpage-oil/src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './Header'
 import PrincipalSection from './components/PrincipalSection'
+import AboutUs from './components/AboutUs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,10 +15,10 @@ function App() {
       <div className="container1 bg-image-hero bg-cover bg-center h-screen">
         <Header />
         <PrincipalSection />
-
       </div>
-
-      <h1 className='font-bold text-3xl text-center bg-red-500'>Hello, Vite + React!</h1>
+      <div className="aboutus">
+        <AboutUs />
+      </div>
     </>
   )
 }
--- landingpage-oil/src/components/AboutUs.jsx
+++ landingpage-oil/src/components/AboutUs.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import image1 from '../img/example-about1.png'
+import image2 from '../img/example-about2.png'
+import image3 from '../img/example-about-3.png'
 
 const AboutUs = () => {
     return (
-        <div>AboutUs</div>
+        <div className='container-aboutus flex justify-around items-center gap-4 mt-11 mb-11'>
+            <div className="images-about grid grid-cols-2 w-1/2">
+                <div className="overflow-hidden flex flex-col gap-3 pr-3">
+                    <img src={image1} alt="About Us 1" />
+                    <img src={image2} alt="About Us 2" />
+                </div>
+                <div className="overflow-hidden">
+                    <img src={image3} alt="About Us 3" className='h-full w-full object-cover' />
+                </div>
+            </div>
+            <div className="text-about w-2/6">
+                <h1 className='text-4xl font-bold mb-2.5'>About Us</h1>
+                <p className='text-base'>It's time to make Fuel stand out! Present the most important information about your brand and show what makes it truly unique. Make it clear what people can expect from Fuel and what value you bring to their lives. Whether it's energy, innovation, or attitude — now is the moment to let your Fuel speak for itself. It's time to make Fuel stand out!
+
+                    Present the most important information about your brand and show what makes it truly unique.</p>
+            </div>
+
+        </div>
     )
 }
 
